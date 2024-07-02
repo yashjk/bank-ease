230601c0baf0ad119d4edcc61bcee1d0e592a568
yash <injose.joshi@gmail.com>
2024-07-02 18:19:27 +0530

    Add right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,38 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
 	const loggedIn = {
-		firstName: "Yash"
+		firstName: "Yash",
+		lastName: "Joshi",
+		email: "injose.joshi@gmail.com",
 	};
 	return (
 		<section className="home">
 			<div className="home-content">
-				<div className="home-header">
+				<header className="home-header">
 					<HeaderBox
 						type="greeting"
 						title="Welcome"
 						user={loggedIn?.firstName || "Guest"}
 						subtext="Access and manage your account and transactions efficiently."
 					/>
-          <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={10111.11}
-          />
-				</div>
+					<TotalBalanceBox
+						accounts={[]}
+						totalBanks={1}
+						totalCurrentBalance={10111.11}
+					/>
+				</header>
+				RECENT TRANSACTIONS
 			</div>
+
+			<RightSidebar
+				user={loggedIn}
+				transactions={[]}
+				banks={[{ currentBalance: 123123 }, { currentBalance: 1000 }]}
+			/>
 		</section>
 	);
 };
