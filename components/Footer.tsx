d2543d79bfb58938717a867f6d19315cd76628d3
yash <injose.joshi@gmail.com>
2024-07-04 23:44:41 +0530

    Display real banking data

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { logoutAccount } from "@/lib/actions/user.action";
+import { logoutAccount } from "@/lib/actions/user.actions";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -15,13 +15,13 @@ const Footer = ({ user, type = "desktop" }: FooterProps) => {
 	return (
 		<footer className="footer">
 			<div className={type === "mobile" ? "footer_name-mobile" : "footer_name"}>
-				<p className="text-xl font-bold text-gray-700">{user?.name[0]}</p>
+				<p className="text-xl font-bold text-gray-700">{user?.firstName[0]}</p>
 			</div>
 			<div
 				className={type === "mobile" ? "footer_email-mobile" : "footer_email"}
 			>
 				<h1 className="text-14 truncate text-gray-700 font-semibold">
-					{user?.name}
+					{user?.firstName}
 				</h1>
 				<p className="text-14 truncate font-normal text-gray-600">
 					{user?.email}
