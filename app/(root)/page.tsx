cd90128531e7c3a5e7ee5557394ccd4ef4a7cd46
yash <injose.joshi@gmail.com>
2024-07-09 03:53:06 +0530

    Add spending categories

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -43,7 +43,7 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 
 			<RightSidebar
 				user={loggedIn}
-				transactions={accounts?.transactions}
+				transactions={account?.transactions}
 				banks={accountsData?.slice(0, 2)}
 			/>
 		</section>
